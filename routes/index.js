3c0c9494663486e7fa4598dd809c86b0af91c54a
novalmahardhika <asubasa9@gmail.com>
2023-11-21 16:23:10 +0700

    feat: grouping routes

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+const { Router } = require('express')
+const router = Router()
+const authRoutes = require('./user.routes')
+const notFoundRoute = require('./404.routes')
+const rootRouter = require('./root.routes')
+
+// swagger
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('../DOCS/openapi.json')
+
+// tes ping router
+router.use('/', rootRouter)
+
+// routes controller
+router.use('/api/v1/', authRoutes)
+
+// api documentation
+router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
+// 404 router
+router.use('*', notFoundRoute)
+
+module.exports = router
