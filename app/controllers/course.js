3d2cfe17c3d95e60198e6db2e32c9ecf73ec8e34
TheReVeaLz <202110715072@mhs.ubharajaya.ac.id>
2023-11-29 16:19:49 +0700

    feat: countModule

--- app/controllers/course.js
+++ app/controllers/course.js
@@ -1,9 +1,26 @@
 const courseService = require('../services/course')
 
+const countModule = (course) => {
+    const chapterLen = Object.keys(course.chapters).length;
+    let totalModule = 0;
+    for(let i = 0; i < chapterLen; i++) {
+        const moduleLen = Object.keys(course.chapters[i].modules).length;
+        for(let j = 0; j < moduleLen; j++) totalModule++;
+    }
+    return totalModule;
+}
+
 const list = async (req, res) => {
     try {
         const isFilter = Object.keys(req.query).length;
         const data = (isFilter) ? await courseService.findAll({ ...req.query }) : await courseService.findAll()
+        const dataLen = Object.keys(data).length;
+
+        for(let i = 0; i < dataLen; i++) {
+            data[i].dataValues.totalModule = countModule(data[i]);
+            delete data[i].dataValues.chapters;
+        }
+        
         res.status(200).json({
             status: 'OK',
             message: 'Success',
@@ -71,13 +88,7 @@ const destroyById = async (req, res) => {
 }
 
 const detail = (req, res) => {
-    const course = req.course;
-    const chapterLen = Object.keys(course.chapters).length;
-    let totalModule = 0;
-    for(let i = 0; i < chapterLen; i++) {
-        const moduleLen = Object.keys(course.chapters[i].modules).length;
-        for(let j = 0; j < moduleLen; j++) totalModule++;
-    }
+    const totalModule = countModule(req.course);
     
     res.json({ status: 'OK', message: 'Success', data: { totalModule, ...req.course.dataValues } })
 }
