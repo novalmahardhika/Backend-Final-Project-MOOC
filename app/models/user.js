5b2a0e1b93d685de3b4f45c51ccf86fef612c98c
novalmahardhika <asubasa9@gmail.com>
2023-12-02 15:01:44 +0700

    feat: added table Otp and create assosiation table otp and user

--- app/models/user.js
+++ app/models/user.js
@@ -9,6 +9,14 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
+
+      static associate(models) {
+      User.hasOne(models.Otp, {
+        foreignKey: 'userId',
+        as: 'OTP'
+      })
+    }
+
   }
   User.init({
     name: DataTypes.STRING,
