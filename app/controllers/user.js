e77e4f0007ee2ea4af8b2eceece507266bb00203
novalmahardhika <asubasa9@gmail.com>
2023-12-07 21:28:51 +0700

    fix: debugging conflict in user controller

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -89,31 +89,7 @@ const verifyAccount = async (req, res) => {
     }
 }
 
-const resendOtp = async (req, res) => {
-    try {
-        res.setHeader('Content-Type', 'application/json');
-        await userService.sendOtp(req.query);
-        res.status(201).json({ status: "OK", message: "Success" });
-    } catch (err) {
-        res.status(err.statusCode).json({
-            status: "FAIL",
-            message: err.message
-        });
-    }
-}
 
-const verifyAccount = async (req, res) => {
-    try {
-        res.setHeader('Content-Type', 'application/json');
-        const user = await userService.verifyAccount(req.query);
-        res.status(201).json({ status: "OK", message: "Account is verified successfully.", data: { user: user.email } });
-    } catch (err) {
-        res.status(err.statusCode).json({
-            status: "FAIL",
-            message: err.message
-        });
-    }
-}
 
 const registerAdmin = async (req, res) => {
     try {
@@ -162,5 +138,7 @@ module.exports = {
     verifyAccount,
     registerAdmin,
     login,
+    setPasswordByOtp,
+    forgotPassword,
     currentUser
 }
