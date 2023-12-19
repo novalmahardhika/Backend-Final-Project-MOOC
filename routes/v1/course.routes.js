320104574dbc5c39f8dc5f104708702f5caa329f
novalmahardhika <asubasa9@gmail.com>
2023-12-19 13:44:18 +0700

    fix: add comment and fixed space

--- routes/v1/course.routes.js
+++ routes/v1/course.routes.js
@@ -10,7 +10,7 @@ const UserCourse = require('../../app/controllers/userCourse')
 const { uploadToMemory } = require('../../middlewares/uploadOnMemory.js');
 const { uploadToCloudinary } = require("../../middlewares/uploadOnCloudinary.js")
 
-// Get ist
+// Get List
 router.get("/courses", Course.list)
 
 // Create new data
@@ -28,20 +28,39 @@ router.get("/courses/:id", AuthMiddleware.authorize2, Course.findAndSetById, Use
 
 
 //  chapter
+
+// Get List Chapter
 router.get("/chapters", Chapter.findAll )
+
+// Get Detail Chapter
 router.get("/chapters/:chapterId", Chapter.findByPk, Chapter.detail )
+
+// Update Chapter
 router.put("/chapters/:chapterId", Chapter.findByPk, Chapter.update )
+
+// Delete Chapter
 router.delete("/chapters/:chapterId", Chapter.findByPk, Chapter.destroy )
 // create chapter, why using endpoint course ? because we need assign course id as a value for field courseId
+
+// Create Chapter
 router.post("/courses/:id", Chapter.create )
 
 
 //  module
+// Get List Module
 router.get("/modules", Module.findAll )
+
+// Get Detail Module
 router.get("/modules/:moduleId", Module.findSetById, Module.detail )
+
+// Update Module
 router.put("/modules/:moduleId", Module.update )
+
+// Delete Module
 router.delete("/modules/:moduleId",  Module.findSetById ,Module.destroy )
 // create module, why using endpoint chapter ? because we need assign chapter id as a value for field chapterId
+
+// Create Module
 router.post("/chapters/:chapterId", Module.create )
 
 
--- routes/v1/index.js
+++ routes/v1/index.js
@@ -11,7 +11,7 @@ const rootRouter = require('./root.routes')
 
 
 // tes ping router
-router.use('/', rootRouter)
+router.use('/v1', rootRouter)
 
 // routes controller
 router.use('/v1/', authRoute)
